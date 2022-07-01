a9c1cf3fbb2a9e637bca858cedde1182f6260d0c
NVB9ALT <102188956+NVB9ALT@users.noreply.github.com>
2022-06-30 20:59:25 -0700

    Delete Lag reducer.js

--- Lag reducer.js
+++
@@ -1,5 +0,0 @@
-//Settings modification
-lagReductionInterval = setInterval(function(){
-   geofs.api.renderingSettings.degradedCollisions = true
-   geofs.api.renderingSettings.lowResRunways = true
-}, 100)
