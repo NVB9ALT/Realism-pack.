7bb0e05ee05846ad1c6ac56e0443ec11ad6bfeb8
NVB9ALT <102188956+NVB9ALT@users.noreply.github.com>
2022-07-08 17:22:05 -0700

    Remove prop physics, add better spin fixer support

--- Realism pack.js
+++ Realism pack.js
@@ -14,8 +14,13 @@ function runSpoilersArming() {
 
 function gBlackout() {if (geofs.animation.values.accZ >= 90) {ui.showCrashNotification();geofs.camera.animations.orbitHorizontal.active = !1}else{ui.hideCrashNotification()}};blackoutInt = setInterval(function(){gBlackout();},10);
 
-function fixSpin() {var pitch = geofs.animation.values.atilt;setTimeout(() => {if ((geofs.animation.values.atilt + 50 < pitch) || (geofs.animation.values.atilt - 50 > pitch)) {geofs.aircraft.instance.definition.minimumSpeed = 600;console.log("Spin detected");geofs.flyToCamera();console.log("Spin fixed");setTimeout(() => {geofs.aircraft.instance.definition.minimumSpeed = 250}, 5000)}}, 500)};let fixyFixy = setInterval(function(){fixSpin()}, 1000);
+function fixSpin() {
+if (geofs.aircraft.instance.id == 2948|| geofs.aircraft.instance.id == 2581) {var pitch = geofs.animation.values.atilt;setTimeout(() => {if ((geofs.animation.values.atilt + 50 < pitch) || (geofs.animation.values.atilt - 50 > pitch)) {geofs.aircraft.instance.definition.minimumSpeed = 600;console.log("Spin detected");geofs.flyToCamera();console.log("Spin fixed");setTimeout(() => {geofs.aircraft.instance.definition.minimumSpeed = 250}, 5000)}}, 500)};
+if (geofs.aircraft.instance.id == 2808 || geofs.aircraft.instance.id == 3460) {var pitch = geofs.animation.values.atilt;setTimeout(() => {if ((geofs.animation.values.atilt + 50 < pitch) || (geofs.animation.values.atilt - 50 > pitch)) {geofs.aircraft.instance.definition.minimumSpeed = 200;console.log("Spin detected");geofs.flyToCamera();console.log("Spin fixed");setTimeout(() => {geofs.aircraft.instance.definition.minimumSpeed = 200}, 5000)}}, 500)};
+if (geofs.aircraft.instance.id == 2988) {var pitch = geofs.animation.values.atilt;setTimeout(() => {if ((geofs.animation.values.atilt + 50 < pitch) || (geofs.animation.values.atilt - 50 > pitch)) {geofs.aircraft.instance.definition.minimumSpeed = 1000;console.log("Spin detected");geofs.flyToCamera();console.log("Spin fixed");setTimeout(() => {geofs.aircraft.instance.definition.minimumSpeed = 250}, 5000)}}, 500)};
+}
+let fixyFixy = setInterval(function(){fixSpin()}, 1000);
 
-var script2 = document.createElement('script'); script2.src="https://cdn.jsdelivr.net/gh/NVB9ALT/GeoFS-Aircraft-Changes@main/Aircraft-fixesF.js";document.body.appendChild(script2);script2.onload = function(){realismify()};
+var script2 = document.createElement('script'); script2.src="https://cdn.jsdelivr.net/gh/NVB9ALT/GeoFS-Aircraft-Changes@main/Aircraft-fixesA.js";document.body.appendChild(script2);script2.onload = function(){realismify()};
 
 })();
