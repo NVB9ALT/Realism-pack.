13fb69995ac4bbe0d21545b91c58d26f885c33d3
NVB9ALT <102188956+NVB9ALT@users.noreply.github.com>
2022-07-16 12:57:40 -0700

    Create Bookmarklet FMC.js

---
+++ Bookmarklet FMC.js
@@ -0,0 +1,1 @@
+javascript:(() => {var scriptFMC = document.createElement('script'); scriptFMC.src="https://cdn.jsdelivr.net/gh/NVB9ALT/Bookmarklet_AP-Plus-Plus_and_FMC@main/fmcE.js";document.body.appendChild(scriptFMC);scriptFMC.onload = function(){runFMC()};})();
