ae382300c2ba10e4f1f4daf489968f3a77eaa927
NVB9ALT <102188956+NVB9ALT@users.noreply.github.com>
2022-07-15 20:19:20 -0700

    Add lift-based CC wingflex

--- Realism pack.js
+++ Realism pack.js
@@ -4,9 +4,13 @@ lagReductionInterval = setInterval(function(){geofs.api.renderingSettings.degrad
 
 var script1 = document.createElement('script'); script1.src="https://cdn.jsdelivr.net/gh/NVB9ALT/Bookmarklet_AP-Plus-Plus@main/ap++.js"; document.body.appendChild(script1);script1.onload = function(){runAPplusplus()};
 
-function runBladeCollisions() {if (geofs.aircraft.instance.id == 9 || geofs.aircraft.instance.id == 52 || geofs.aircraft.instance.id == 4090) {
+function runBladeCollisions() {if (geofs.aircraft.instance.id == 9 || geofs.aircraft.instance.id == 52 || geofs.aircraft.instance.id == 2840 || geofs.aircraft.instance.id == 4090) {
 /*code here*/
 }};bladeCollisionInterval = setInterval(function(){runBladeCollisions()}, 1000);
+
+function runSensitivityFix() {if (geofs.aircraft.instance.id == 9 || geofs.aircraft.instance.id == 52 || geofs.aircraft.instance.id == 2840 || geofs.aircraft.instance.id == 4090) {
+/*code here*/
+}};sensitivityFixInterval = setInterval(function(){runSensitivityFix()}, 1000);
 
 function runCarrierCatapults() {
 ui.notification.show("Use q to lock the launch bar.");let catLlas=[[37.778307623586805,-122.6090264835742,22.753097613256113]],carrierPlaneIds=["7","2581","3460"],barDown=!0,barLocked=!1,launchKey="`",lockKey="q";function gearBarPosLock(){barLocked&&geofs.aircraft.instance.rigidBody.setLinearVelocity([0,0,0])}function resolveForceVector(e,a){return fx=e*Math.cos(a*(Math.PI/180)),fy=e*Math.sin(a*(Math.PI/180)),[fx,fy,0]}function distance(e,a){var t=a[0]-e[0],n=a[1]-e[1],o=a[2]-e[2];return Math.sqrt(t*t+n*n+o*o)}carrierPlaneIds.includes(geofs.aircraft.instance.id)&&document.addEventListener("keypress",function(e){if(e.key===lockKey&&(barLocked?(barLocked=!1,clearInterval(lockInt),ui.notification.show("Launch Bar Unlocked")):catLlas.forEach(function(e){distance(geofs.aircraft.instance.llaLocation,e)<10&&(barLocked=!0,ui.notification.show("Launch bar locked, apply max power and press ~ to launch."),lockInt=setInterval(function(){gearBarPosLock()}))})),e.key===launchKey&&barLocked&&1==geofs.animation.values.throttle){clearInterval(lockInt),barLocked=!1,barDown=!1,geofs.aircraft.instance.rigidBody.reset();var a=10*geofs.aircraft.instance.rigidBody.mass;let e=new geofs.fx.ParticleEmitter({anchor:{worldPosition:[0,0,-1]},duration:1e5,rate:.05,life:4e4,easing:"easeOutQuart",startScale:5e-4,endScale:5e-4,randomizeStartScale:.05,randomizeEndScale:.15,startOpacity:.9,endOpacity:1e-5,startRotation:"random",texture:"whitesmoke"});launchInterval=setInterval(function(){1==geofs.animation.values.groundContact?geofs.aircraft.instance.rigidBody.applyCentralImpulse([resolveForceVector(a,geofs.animation.values.heading360)[1],resolveForceVector(a,geofs.animation.values.heading360)[0],resolveForceVector(a,geofs.animation.values.heading360)[2]]):(clearInterval(launchInterval),e.destroy())},200)}});
@@ -25,7 +29,8 @@ if (geofs.aircraft.instance.id == 2988) {var pitch = geofs.animation.values.atil
 }
 let fixyFixy = setInterval(function(){fixSpin()}, 1000);
 
-var script2 = document.createElement('script'); script2.src="https://cdn.jsdelivr.net/gh/NVB9ALT/GeoFS-Aircraft-Changes@main/Aircraft-fixesE.js";document.body.appendChild(script2);script2.onload = function(){realismify()};
+var aircraftChecked = new Boolean(0)
+var script2 = document.createElement('script'); script2.src="https://cdn.jsdelivr.net/gh/NVB9ALT/GeoFS-Aircraft-Changes@main/Aircraft-fixesX.js";document.body.appendChild(script2);script2.onload = function(){realismify()};
 
 function showTheStars() {if (geofs.aircraft.instance.altitude >= 80000 || geofs.isNight == 1) {geofs.api.viewer.scene.skyBox.show = 1}else {geofs.api.viewer.scene.skyBox.show = 0}};starsInterval = setInterval(function(){showTheStars()}, 1000);
 
