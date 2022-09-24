b0da2c1d6b355ed3b404f39355c13c5cebf80476
NVB9ALT <102188956+NVB9ALT@users.noreply.github.com>
2022-09-23 17:38:30 -0700

    Add night scenery and cockpit lighting

--- mainWithLiverySelector.js
+++ mainWithLiverySelector.js
@@ -433,4 +433,21 @@ instruments.renderers.genericHUD = function (a) {
         d.textAlign = "left";
         d.fillText("G " + geofs.animation.getValue("loadFactor").toFixed(1), 143, 110);
     }
+function checkNightStuff() {
+   if (geofs.isNight == 1 && geofs.api.renderingSettings.advancedAtmosphere == 0 && geofs.camera.currentModeName !== "cockpit") {
+geofs.api.setImageryBrightness(75);
+   };
+   if (geofs.isNight == 1 && geofs.api.renderingSettings.advancedAtmosphere == 0 && geofs.camera.currentModeName == "cockpit") {
+geofs.api.setImageryBrightness(50);
+   };
+   if (geofs.isNight == 0 && weather.definition.cloudCover >= 80 && (geofs.preferences.graphics.volumetricClouds == false || geofs.api.renderingSettings.advancedAtmosphere == 0)) {
+geofs.api.viewer.scene.light.intensity = weather.definition.cloudCover / 80
+   };
+   if (geofs.camera.currentModeName == "cockpit" && geofs.isNight == 1) {
+geofs.api.viewer.scene.light.intensity = 0.5;
+geofs.api.viewer.scene.light.color = {red: 0.8, green: 0.63, blue: 0.52, alpha: 1};
+	} else if (geofs.isNight == 1) {
+geofs.api.viewer.scene.light.intensity = 0.1
+	}
+};checkMarbleInterval = setInterval(function(){checkNightStuff()},10)
 }
