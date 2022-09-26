0a1b2ca265612f323a75c7bc2cd11e5f27db00c7
NVB9ALT <102188956+NVB9ALT@users.noreply.github.com>
2022-09-26 06:50:38 -0700

    Fix cockpit lighting bug

--- main.js
+++ main.js
@@ -440,10 +440,10 @@ geofs.api.setImageryBrightness(150);
    if (geofs.isNight == 1 && geofs.api.renderingSettings.advancedAtmosphere == 0 && geofs.camera.currentModeName == "cockpit") {
 geofs.api.setImageryBrightness(50);
    };
-   if (geofs.camera.currentModeName == "cockpit" && geofs.isNight == 1) {
+   if (geofs.camera.currentModeName == "cockpit" && geofs.isNight == 1 && geofs.api.renderingSettings.advancedAtmosphere == 0) {
 geofs.api.viewer.scene.light.intensity = 0.5;
 geofs.api.viewer.scene.light.color = {red: 0.8, green: 0.63, blue: 0.52, alpha: 1};
-	} else if (geofs.isNight == 1) {
+	} else if (geofs.isNight == 1 && geofs.api.renderingSettings.advancedAtmosphere == 0) {
 geofs.api.viewer.scene.light.intensity = 0.5
 	}
 };checkMarbleInterval = setInterval(function(){checkNightStuff()},10)
