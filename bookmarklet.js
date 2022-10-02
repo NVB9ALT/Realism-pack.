89eea07d7367b32f675888c780c3e3f6a4368c9d
NVB9ALT <102188956+NVB9ALT@users.noreply.github.com>
2022-10-02 09:06:12 -0700

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,5 +1,1 @@
-//Regular version
 javascript:(() => {var realismScript = document.createElement('script'); realismScript.src="https://raw.githack.com/NVB9ALT/Realism-pack./main/main.js";document.body.appendChild(realismScript);realismScript.onload = (function(){realismGo()});})()
-
-//Version that includes livery selector by default (incompatible with challenges addon or similar)
-javascript:(() => {var realismScript = document.createElement('script'); realismScript.src="https://raw.githack.com/NVB9ALT/Realism-pack./main/mainWithLiverySelector.js";document.body.appendChild(realismScript);realismScript.onload = (function(){realismGo()});})()
