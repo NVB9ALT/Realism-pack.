c22cba8ed52c3c57e7c7d2b3917e439c7359dfab
NVB9ALT <102188956+NVB9ALT@users.noreply.github.com>
2022-11-22 14:19:46 -0800

    Fix fighter jet ejection seats not working

--- main.js
+++ main.js
@@ -183,7 +183,7 @@ function realismGo() {
         autospoilers();
     };
     var scriptEJ = document.createElement("script");
-    scriptEJ.src = "https://cdn.jsdelivr.net/gh/NVB9ALT/Fighter-jet-ejections@main/mainE.js";
+    scriptEJ.src = "https://cdn.jsdelivr.net/gh/NVB9ALT/Fighter-jet-ejections@main/mainG.js";
     document.body.appendChild(scriptEJ);
     scriptEJ.onload = function () {
         runEjections();
