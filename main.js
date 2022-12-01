1fec4d9955b167431c593e22f0728ef6bca43ede
NVB9ALT <102188956+NVB9ALT@users.noreply.github.com>
2022-12-01 08:49:23 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1199,8 +1199,10 @@ function realismGo() {
       if (geofs.addonAircraft.isMiG21 == 1 && controls.optionalAnimatedPart.target == 1) {
           geofs.debug.loadMig21Tank()
       }
-      if (geofs.addonAircraft.isMSG == 1 && geofs.animation.values.view != "cockpit" && geofs.animation.values.enginesOn == 1) {
+      if (geofs.addonAircraft.isMSG == 1 && geofs.animation.values.view != "cockpit") {
           geofs.debug.loadMSG();
+      }
+      if (geofs.addonAircraft.isMSG == 1 && geofs.animation.values.view != "cockpit" && geofs.animation.values.enginesOn == 0) {
           geofs.debug.loadMSGprop();
       }
         
