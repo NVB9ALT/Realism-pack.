d9b9649f7045b0bc241dd88af664bd5721047484
NVB9ALT <102188956+NVB9ALT@users.noreply.github.com>
2023-02-21 18:37:36 -0800

    Cleanup

--- main.js
+++ main.js
@@ -675,20 +675,6 @@ clearInterval(blackoutLoadInt)
             d.textAlign = "left";
             d.fillText("G " + geofs.animation.getValue("loadFactor").toFixed(1), 143, 110);
         }
-    function checkNightStuff() {
-       if (geofs.isNight == 1 && geofs.api.renderingSettings.advancedAtmosphere == 0 && geofs.camera.currentModeName !== "cockpit") {
-    geofs.api.setImageryBrightness(150);
-       };
-       if (geofs.isNight == 1 && geofs.api.renderingSettings.advancedAtmosphere == 0 && geofs.camera.currentModeName == "cockpit") {
-    geofs.api.setImageryBrightness(50);
-       };
-       if (geofs.camera.currentModeName == "cockpit" && geofs.isNight == 1 && geofs.api.renderingSettings.advancedAtmosphere == 0) {
-    geofs.api.viewer.scene.light.intensity = 0.5;
-    geofs.api.viewer.scene.light.color = {red: 0.8, green: 0.63, blue: 0.52, alpha: 1};
-        } else if (geofs.isNight == 1 && geofs.api.renderingSettings.advancedAtmosphere == 0) {
-    geofs.api.viewer.scene.light.intensity = 0.5
-        }
-    };checkMarbleInterval = setInterval(function(){checkNightStuff()},10)
     
     geofs.condensation = {};
     geofs.cons = true;
