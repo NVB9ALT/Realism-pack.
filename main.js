cfb87ff851bc7cb63f40a4c330de8045b9f58273
NVB9ALT <102188956+NVB9ALT@users.noreply.github.com>
2024-01-03 20:13:35 -0800

    Update main.js

--- main.js
+++ main.js
@@ -334,7 +334,7 @@ clearInterval(blackoutLoadInt)
         lookBack();
     }, 100);
     var script2 = document.createElement("script");
-    script2.src = "https://cdn.jsdelivr.net/gh/NVB9ALT/GeoFs-Carrier-Catapults-from-AriakimTaiyo@main/catapultsZ.js";
+    script2.src = "https://cdn.jsdelivr.net/gh/NVB9ALT/GeoFs-Carrier-Catapults-from-AriakimTaiyo@main/catapultsY.js";
     document.body.appendChild(script2);
     script2.onload = function () {
         runCatapults();
