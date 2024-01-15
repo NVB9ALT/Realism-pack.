015e952f66f257af6ed6ab5dac7e461122830a6d
NVB9ALT <102188956+NVB9ALT@users.noreply.github.com>
2024-01-15 12:40:13 -0800

    Fix spoiler arming keybind

--- main.js
+++ main.js
@@ -318,9 +318,65 @@ loadInterval = setInterval(function(){
     scriptEJ.onload = function () {
         runEjections();
     };
-    var scriptAS = document.createElement("script");
-    scriptAS.src = "https://raw.githack.com/kolos26/GeoFS-spoilerArming/main/spoilerarming.js";
-    document.body.appendChild(scriptAS);
+
+geofs.aircraft.instance.animationValue.spoilerArming = 0
+
+controls.setters.setSpoilerArming = {
+    label: "Spoiler Arming",
+    set: function () {
+        if (!geofs.aircraft.instance.groundContact && controls.airbrakes.position === 0){
+        geofs.aircraft.instance.animationValue.spoilerArming = 1
+        }
+    },
+};
+
+controls.setters.setAirbrakes= {
+    label: "Air Brakes",
+    set: function () {
+        controls.airbrakes.target = 0 == controls.airbrakes.target ? 1 : 0;
+        controls.setPartAnimationDelta(controls.airbrakes);
+        geofs.aircraft.instance.animationValue.spoilerArming = 0
+    },
+}
+
+instruments.definitions.spoilers.overlay.overlays[3] = {
+    anchor: { x: 0, y: 0 },
+    size: { x: 50, y: 50 },
+    position: { x: 0, y: 0 },
+    animations: [{ type: "show", value: "spoilerArming", when: [1] }],
+    class: "control-pad-dyn-label green-pad",
+    text: "SPLR<br/>ARM",
+    drawOrder: 1
+};
+
+instruments.init(geofs.aircraft.instance.setup.instruments)
+
+$(document).keydown(
+    function (e) {
+        if (e.which == 16 && e.which == 66){ //spoiler arming key is "shift+B"
+            controls.setters.setSpoilerArming.set()
+        }
+    }
+)
+
+setInterval(
+    function(){
+        if(geofs.aircraft.instance.animationValue.spoilerArming === 1 && geofs.aircraft.instance.groundContact && controls.airbrakes.position === 0){
+            controls.setters.setAirbrakes.set();
+            geofs.aircraft.instance.animationValue.spoilerArming = 0;
+        }
+    },
+100)
+
+//add spoiler indicator for those planes that do not have it by themselves
+setInterval(
+    function(){
+        if(["3292", "3054"].includes(geofs.aircraft.instance.id) && geofs.aircraft.instance.setup.instruments["spoilers"] === undefined){
+            geofs.aircraft.instance.setup.instruments["spoilers"] = "";
+            instruments.init(geofs.aircraft.instance.setup.instruments);
+        }
+    },
+500)
 
     var scriptKCAS = document.createElement("script");
     scriptKCAS.src = "https://raw.githack.com/NVB9ALT/Bookmarklet_AP-Plus-Plus_and_FMC/main/Realistic%20KIAS.js";
